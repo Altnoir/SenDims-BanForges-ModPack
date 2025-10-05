984d02b7ae656a79b5b48fff308e2c87721297d5
Tonywww <1204484415@qq.com>
2025-10-06 00:47:46 +0800

    任务

--- kubejs/server_scripts/recipes/s1.js
+++ kubejs/server_scripts/recipes/s1.js
@@ -11,12 +11,15 @@ ServerEvents.recipes(event => {
         C: 'minecraft:netherite_ingot'
     }).id('sdbf:apotheosis_library_s1')
 
-    // event.shaped($StructureQuill.forStructure("blue_skies:everdawn_blinding_dungeon"), [
-    //     'AAA',
-    //     'ABA',
-    //     'AAA'
-    // ], {
-    //     A: 'blue_skies:lunar_stonebrick',
-    //     B: 'minecraft:map'
-    // }).id('sdbf:sq_everdawn_blinding_dungeon_s1')
+        event.shaped('kubejs:garden_lighter', [
+        'A  ',
+        ' B ',
+        ' CD'
+    ], {
+        A: 'dustandash:bloody_flint',
+        B: 'dustandash:sharpen_flint',
+        C: 'minecraft:flowering_azalea',
+        D: 'minecraft:flint'
+    }).id('sdbf:garden_lighter_s1')
+
 })
--- kubejs/startup_scripts/reg.js
+++ kubejs/startup_scripts/reg.js
@@ -7,5 +7,7 @@ StartupEvents.registry('item', event => {
     event.create('gamma_dust').fireResistant().rarity('rare')
     event.create('delta_dust').fireResistant().rarity('epic')
     event.create('epsilon_dust').fireResistant().rarity('epic')
+    
+    event.create('garden_lighter').fireResistant().rarity('rare')
 
 })
