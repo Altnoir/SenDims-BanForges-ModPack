c997cdb53371911539322686a60f126d6447ff0f
Tonywww <1204484415@qq.com>
2025-10-28 01:14:19 +0800

    blades

--- kubejs/server_scripts/recipes/s0_garden.js
+++ kubejs/server_scripts/recipes/s0_garden.js
@@ -1,7 +1,12 @@
 ServerEvents.recipes(event => {
     event.smelting('#forge:gems/ruby', ['thermal_shock:rough_ruby'])
-        .id("sdbf:ruby_s0")
+        .id("sdbf:ruby_s0");
     event.smelting('thermal_shock:bloodstone', ['thermal_shock:rough_bloodstone'])
-        .id("sdbf:bloodstone_s0")
+        .id("sdbf:bloodstone_s0");
+
+    event.replaceInput({id: "cialloblade:ciallo"},
+        'minecraft:cake',
+        'minecraft:stripped_birch_log'
+    )
 
 })
